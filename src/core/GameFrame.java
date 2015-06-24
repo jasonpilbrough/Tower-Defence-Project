039412611b4ebaeb07cf85867b871b3a1e4167b4
Jayson2142 <09reebok@gmail.com>
2015-06-24 20:09:23 +0200

    Define main GameFrame class

---
+++ src/core/GameFrame.java
@@ -0,0 +1,39 @@
+package core;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class GameFrame extends JFrame implements ActionListener{
+	private GamePanel gp = new GamePanel();
+	private Timer t = new Timer(40,this);
+	
+	
+	
+	public GameFrame() {
+		t.start();
+	}
+		
+	public static void main(String[] args) {
+    	GameFrame gf = new GameFrame();
+    	
+    	gf.add(gf.gp,BorderLayout.CENTER);
+    	gf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gf.setVisible(true);
+       
+
+    }
+	
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		gp.update(40);
+		gp.repaint();
+		
+	}
+
+}

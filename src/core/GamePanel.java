eab7c68c53db243b1578186dc35d91902e2a0de8
Jayson2142 <09reebok@gmail.com>
2015-06-24 19:28:09 +0200

    Define core GamePanel functions

---
+++ src/core/GamePanel.java
@@ -0,0 +1,47 @@
+package core;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class GamePanel {
+	
+	private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+	
+	
+	public void addGameObject(GameObject go){
+		gameObjects.add(go);
+	}
+	
+	public void deleteGameObject(GameObject go){
+		
+		if(gameObjects.contains(go)){
+			gameObjects.remove(gameObjects.indexOf(go));
+		}else{
+			JOptionPane.showMessageDialog(null, "Could not delete object");
+		}
+	}
+	
+	public void paint(Graphics g){
+		g.setColor(new Color(25, 70, 66));
+		//g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
+		
+		for (GameObject gameObject : gameObjects) {
+			gameObject.paint(g);
+		}
+		
+		
+	}
+	
+	public void update(int deltaTime){
+		for (GameObject gameObject : gameObjects) {
+			gameObject.update(deltaTime);
+		}
+	}
+	
+	
+
+}

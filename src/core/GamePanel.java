d6a5927f8b66e27ec8177159c41222f6cc64bb5a
Jayson2142 <09reebok@gmail.com>
2015-06-26 12:52:23 +0200

    Implement mouse listener in GamePanel

--- src/core/GamePanel.java
+++ src/core/GamePanel.java
@@ -2,21 +2,30 @@ package core;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import bullets.Bullet;
+import towers.Tower;
 import monsters.Monster;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements MouseListener{
 	
 	private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 	private Map map = new Map();
 	
+	public GamePanel(){
+		addMouseListener(this);
+	}
 	
 	public void addGameObject(GameObject go){
 		gameObjects.add(go);
+	
 	}
 	
 	public void deleteGameObject(GameObject go){
@@ -62,6 +71,57 @@ public class GamePanel extends JPanel{
 		for (GameObject gameObject : gameObjects) {
 			gameObject.update(deltaTime);
 		}
+	}
+
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			int tempRow = e.getY() / Block.getHeight();
+			int tempCol = e.getX() / Block.getWidth();
+			double posX = tempCol * Block.getWidth() + Block.getWidth() / 2;
+			double posY =  tempRow * Block.getHeight() + Block.getHeight() / 2;
+			
+
+			Tower t =new Tower(posX, posY,20.0,200.0,10.0,5.0,Toolkit.getDefaultToolkit().getImage("images/redTower.png"),this);
+			
+			if (map.placeTower(tempRow, tempCol, t)) {
+				addGameObject(t);
+			}
+			
+		}
+		
+		if (e.getButton() == MouseEvent.BUTTON3) {
+			addGameObject(new Monster(15, map.getPath(), 50,100,this));
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	

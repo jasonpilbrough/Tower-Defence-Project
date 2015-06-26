5cebf29be195b025a4fe69c3dba29acbc6e7d7b5
Jayson2142 <09reebok@gmail.com>
2015-06-26 13:28:34 +0200

    Remove commenting on GamePanel

--- src/core/GamePanel.java
+++ src/core/GamePanel.java
@@ -17,77 +17,15 @@ import monsters.Monster;
 
 public class GamePanel extends JPanel implements MouseListener{
 	
+	private GameObject[] gameObjects = new GameObject[10000] ;
+	private int gameObjectsCount;
+	
 	
 	private Map map = new Map();
 	
 	public GamePanel(){
 		addMouseListener(this);
 	}
-	
-	
-	/*
-	private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
-	
-	public void addGameObject(GameObject go){
-		gameObjects.add(go);
-	
-	}
-	
-	public void deleteGameObject(GameObject go){
-		
-		if(gameObjects.contains(go)){
-			//int index = gameObjects.indexOf(go);
-			gameObjects.remove(go);
-		}else{
-			JOptionPane.showMessageDialog(null, "Could not delete object");
-		}
-	}
-	
-	public int getGameObjectArrayLength(){
-		return gameObjects.size();
-	}
-	
-	public ArrayList<Monster> getMonsters(){
-		ArrayList<Monster> m = new ArrayList<Monster>();
-		
-		
-		for (GameObject gameObject : gameObjects) {
-			if(gameObject instanceof Monster){
-				m.add((Monster)gameObject);
-			}
-		}
-		
-		
-		return m;
-	}
-	
-	public void paint(Graphics g){
-		g.setColor(new Color(25, 70, 66));
-		//g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		
-		map.paint(g);
-		for (GameObject gameObject : gameObjects) {
-			gameObject.paint(g);
-		}
-		
-		
-	}
-	
-	public void update(int deltaTime){
-		try {
-			for (GameObject gameObject : gameObjects) {
-				gameObject.update(deltaTime);
-			}
-		} catch (Exception e) {
-			//JOptionPane.showMessageDialog(null, gameObjects);
-		}
-	}
-	*/
-	
-	private GameObject[] gameObjects = new GameObject[10000] ;
-	private int gameObjectsCount;
-		
-		
 	
 	public void addGameObject(GameObject go){
 		gameObjects[gameObjectsCount++] = go;
